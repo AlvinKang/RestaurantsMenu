fa5516ab087a65d3b7043ac4c5281d0a97ee1a77
Alvin Kang <oneinallin1@gmail.com>
2017-09-13 14:54:30 -0700

    Add routes for restaurants

--- finalProject.py
+++ finalProject.py
@@ -1,17 +1,31 @@
-
+from flask import Flask
+app = Flask(__name__)
 
 # Show all restaurants
 # /restaurants
 # /
+@app.route('/')
+@app.route('/restaurants/')
+def showRestaurants():
+	return "This page will show all my restaurants"
 
 # Create a new restaurant
 # /restaurant/new
+@app.route('/restaurant/new/')
+def newRestaurant():
+	return "This page will be for making a new restaurant"
 
 # Edit a restaurant
 # /restaurant/<int:restaurant_id>/edit
+@app.route('/restaurant/<int:restaurant_id>/edit/')
+def editRestaurant(restaurant_id):
+	return "This page will be for editing restaurant {}".format(restaurant_id)
 
 # Delete a restaurant
 # /restaurant/<int:restaurant_id>/delete
+@app.route('/restaurant/<int:restaurant_id>/delete/')
+def deleteRestaurant(restaurant_id):
+	return "This page will be for deleting restaurant {}".format(restaurant_id)
 
 #########################################
 
@@ -27,3 +41,7 @@
 
 # Delete a menu item
 # /restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete
+
+if __name__ == '__main__':
+	app.debug == True
+	app.run(host = '0.0.0.0', port = 5000)
