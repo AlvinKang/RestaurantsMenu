c0e264f5e23b2919e1d370de3a7b9c5071e57eb6
Alvin Kang <oneinallin1@gmail.com>
2017-09-14 11:32:17 -0700

    Make and render template for restaurants landing page

--- finalProject.py
+++ finalProject.py
@@ -1,5 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+#Fake Restaurants
+restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+
+restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+
+#Fake Menu Items
+items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
+
+#########################################
 
 # Show all restaurants
 # /restaurants
@@ -7,7 +18,8 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/restaurant/')
 def showRestaurants():
-	return "This page will show all my restaurants"
+	return render_template('restaurants.html', restaurants=restaurants)
+	# return "This page will show all my restaurants"
 
 # Create a new restaurant
 # /restaurant/new
@@ -47,6 +59,7 @@ def newMenuItem(restaurant_id):
 # /restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit/')
 def editMenuItem(restaurant_id, menu_id):
+
 	return "This page is for editing menu item {}".format(menu_id)
 
 # Delete a menu item
@@ -56,5 +69,5 @@ def deleteMenuItem(restaurant_id, menu_id):
 	return "This page is for editing menu item {}".format(menu_id)
 
 if __name__ == '__main__':
-	app.debug == True
+	app.debug = True
 	app.run(host = '0.0.0.0', port = 5000)
