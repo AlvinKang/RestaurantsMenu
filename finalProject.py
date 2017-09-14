386336b2519f351d13914a8afdf21adcd6a77fe2
Alvin Kang <oneinallin1@gmail.com>
2017-09-13 18:41:37 -0700

    Add routes for menu items

--- finalProject.py
+++ finalProject.py
@@ -32,15 +32,28 @@ def deleteRestaurant(restaurant_id):
 # Show a restaurant menu
 # /restaurant/<int:restaurant_id>/menu
 # /restaurant/<int:restaurant_id>
+@app.route('/restaurant/<int:restaurant_id>/menu/')
+@app.route('/restaurant/<int:restaurant_id>/')
+def showMenu(restaurant_id):
+	return "This page is the menu for restaurant {}".format(restaurant_id)
 
 # Create a new menu item
 # /restaurant/<int:restaurant_id>/menu/new
+@app.route('/restaurant/<int:restaurant_id>/menu/new/')
+def newMenuItem(restaurant_id):
+	return "This page is for making a new menu item for restaurant {}".format(restaurant_id)
 
 # Edit a menu item
 # /restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit/')
+def editMenuItem(restaurant_id, menu_id):
+	return "This page is for editing menu item {}".format(menu_id)
 
 # Delete a menu item
 # /restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete/')
+def deleteMenuItem(restaurant_id, menu_id):
+	return "This page is for editing menu item {}".format(menu_id)
 
 if __name__ == '__main__':
 	app.debug == True
